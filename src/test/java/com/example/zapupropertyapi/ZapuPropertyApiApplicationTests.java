943f37ce943f678bab3a3c7948987b1836c0df87
Abdullah Aktas <abdullah.aktas82@gmail.com>
2022-05-22 12:08:20 +0300

    first commit

---
+++ src/test/java/com/example/zapupropertyapi/ZapuPropertyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zapupropertyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapuPropertyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
