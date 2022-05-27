b3807275bd049b56b011e5358a9b761a5d95f51c
Abdullah Aktas <abdullah.aktas82@gmail.com>
2022-05-27 18:25:56 +0300

    start docker when deployment with docker-compose

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
+sudo docker-compose up -d
 sudo java -jar -Dserver.port=80 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
 cd /home/ec2-user/server
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo dockerd
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo docker-compose up -d
